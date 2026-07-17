1eff8096a5f1f6caa6878507bd57ccd726030006
tantunes_cisco <tantunes@cisco.com>
2026-07-17 12:00:15 +0100

    Configure Firebase project

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyA2WCN1r1Qh1TNnZficIfJY1H2K7X04bSE",
+  authDomain: "a-noite-em-que-assinei.firebaseapp.com",
+  databaseURL: "https://a-noite-em-que-assinei-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "a-noite-em-que-assinei",
+  storageBucket: "a-noite-em-que-assinei.firebasestorage.app",
+  messagingSenderId: "980152868562",
+  appId: "1:980152868562:web:3cf36ab5d8515423e0d70b",
+  measurementId: "G-33TSEVD0ZQ"
 };
 
 export const emailNotificationConfig = {
